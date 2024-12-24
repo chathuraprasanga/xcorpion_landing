dbaa6c66a82ce0793f3fd1c34dfe6b42e501f315
Chathura Prasanga <chathuraprasanga98@gmail.com>
2024-12-25 00:37:15 +0530

    lint issues

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,19 @@
-import globals from "globals"
-import pluginJs from "@eslint/js"
-import tseslint from "typescript-eslint"
-import pluginReact, { rules } from "eslint-plugin-react"
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pkg from "eslint-plugin-react";
 
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import("eslint").Linter.Config[]} */
 export default [
     { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
     { languageOptions: { globals: globals.browser } },
     pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
-    pluginReact.configs.flat.recommended,
+    pkg.configs.flat.recommended,
     {
         rules: {
-          "@typescript-eslint/no-explicit-any": "off", // Allow the use of 'any' everywhere
+            "@typescript-eslint/no-explicit-any": "off", // Allow the use of 'any' everywhere
+            "react/react-in-jsx-scope": "off", // Disable this rule for React 17+
         },
     },
-]
+];
